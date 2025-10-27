251d51ee92c4a39210974b61e816a6065fba53d7
Glaxion69 <paddysagar.69@gmail.com>
2025-10-27 23:14:53 +0000

    [refactor] Added Error Handling at main with exit codes

--- generate_keycloak_stream_report.py
+++ generate_keycloak_stream_report.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from dotenv import load_dotenv
 
 from controllers.catalog_keyclock_api import (
@@ -8,17 +9,33 @@ from controllers.catalog_keyclock_api import (
     select_latest_image_per_version,
 )
 
+from utils.helper import setup_logging
+
 load_dotenv()
+logger = setup_logging()
 
 
 def main():
-    keycloak_images_api_data = fetch_keycloak_images_in_catalog()
-    images_grouped_by_version = group_images_by_version_label(keycloak_images_api_data)
-    latest_image_per_version = select_latest_image_per_version(
-        images_grouped_by_version
-    )
-    print(json.dumps(build_version_summary_report(latest_image_per_version), indent=2))
-    return build_version_summary_report(latest_image_per_version)
+    try:
+        logger.info(
+            "[main] Starting Keycloak Content Stream report for most recent Image"
+        )
+
+        keycloak_images_api_data = fetch_keycloak_images_in_catalog()
+        images_grouped_by_version = group_images_by_version_label(
+            keycloak_images_api_data
+        )
+        latest_image_per_version = select_latest_image_per_version(
+            images_grouped_by_version
+        )
+        report = build_version_summary_report(latest_image_per_version)
+
+        print(json.dumps(report, indent=2))
+        logger.info("[main] Report generation completed successfully")
+
+    except Exception as e:
+        logger.error(f"[main] Report generation failed: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
